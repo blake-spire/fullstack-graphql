8f6fa400b7ccf8643dbd66e34370aa5b2eb8abbf
blake <blake.netzeband@spiredigital.com>
2020-04-06 07:46:20 -0600

    create Interface

--- api/demo.js
+++ api/demo.js
@@ -2,23 +2,48 @@ const gql = require("graphql-tag");
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = gql`
+  """
+  this is how you would make a
+  multi-line comment
+  """
+  enum ShoeType {
+    JORDAN
+    NIKE
+    ADIDAS
+    ALLBIRDS
+    BERKENSTOCKS
+  }
+
   type User {
     email: String!
     avatar: String
     friends: [User]!
   }
-  type Shoe {
-    brand: String!
+
+  interface Shoe {
+    brand: ShoeType!
     size: Int!
   }
 
+  type Sneaker implements Shoe {
+    brand: ShoeType!
+    size: Int!
+    color: String
+  }
+
+  type FlipFlop implements Shoe {
+    brand: ShoeType!
+    size: Int!
+    hasHeelStrap: Boolean
+  }
+
   input ShoeInput {
-    brand: String
+    brand: ShoeType
     size: Int
   }
 
   input NewShoeInput {
-    brand: String!
+    brand: ShoeType!
     size: Int!
   }
 
@@ -38,7 +63,7 @@ const resolvers = {
       return {
         email: "yoda@gmail.com",
         avatar: "http://yoda.png",
-        friends: []
+        friends: [],
       };
     },
 
@@ -46,22 +71,29 @@ const resolvers = {
       const { input } = arguments;
 
       return [
-        { brand: "Nike", size: 8 },
-        { brand: "Allbirds", size: 9 }
-      ].filter(shoe => shoe.brand === input.brand);
-    }
+        { brand: "NIKE", size: 8, color: "red" },
+        { brand: "ALLBIRDS", size: 9, color: "black" },
+        { brand: "BERKENSTOCKS", size: 10, hasHeelStrap: false },
+      ];
+    },
   },
 
   Mutation: {
     newShoe(_initialValue, args) {
       return args.input;
-    }
-  }
+    },
+  },
+  Shoe: {
+    __resolveType(shoe) {
+      if (shoe.hasOwnProperty("hasHeelStrap")) return "FlipFlop";
+      else return "Sneaker";
+    },
+  },
 };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 server.listen(4000).then(() => console.log("listening on port 4000"));
